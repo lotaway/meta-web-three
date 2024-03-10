b01cd94e2b512221d35897d980b11201c4b09a1a
lotaway <576696294@qq.com>
2024-03-10 10:34:18 +0800

    Update token contract

--- block-chain-use/contracts/MetaThreeCoin.sol
+++ block-chain-use/contracts/MetaThreeCoin.sol
@@ -1,15 +1,25 @@
-//  SPDX-License-Identifier: UNLICENSED
+//  SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MetaThreeCoin is StandardToken {
+contract MetaThreeCoin is ERC20 {
     string public name = "MetaThreeCoin";
     string public symbol = "BLC";
     uint8 public decimals = 4;
     uint256 public TOKENS = 99_999;
+    address public owner;
 
-    function MetaThreeCoin() {
+    constructor() ERC20("MetaCoinETH", "ECH") {
+        owner = msg.sender;
         totalSupply = TOKENS;
-        balances[msg.sender] = TOKENS;
+        balanceOf[msg.sender] = TOKENS;
+    }
+
+    event SetOwner(address originOwner, address newOwner);
+
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner, "Not owner");
+        SetOwner(owner, newOwner);
+        owner = newOwner;
     }
 }
