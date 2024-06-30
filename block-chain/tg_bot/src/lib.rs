4bdd5b2a14f5a41992115256c75e42265643280c
lotaway <576696294@qq.com>
2024-06-30 16:50:22 +0800

    Update bot

--- block-chain/tg_bot/src/lib.rs
+++ block-chain/tg_bot/src/lib.rs
@@ -3,10 +3,12 @@ use dptree::prelude::*;
 use teloxide::dispatching::UpdateFilterExt;
 use teloxide::prelude::*;
 use teloxide::types::{
-    InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, ReplyMarkup, WebAppInfo,
+    BotCommand, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, ReplyMarkup, WebAppInfo,
 };
 use teloxide::types::{InputFile, Update};
 use teloxide::utils::command::BotCommands;
+
+static COMMAND_START: BotCommand = BotCommand::new("start", "Start the app");
 
 #[derive(BotCommands, PartialEq, Debug)]
 #[command(
@@ -20,73 +22,101 @@ enum UserCommandType {
     Start,
 }
 
-pub async fn run() {
-    // teloxide::handler!();
-    log::info!("Starting telegram bot...");
+struct TGBotProgram {
+    bot: std::sync::Arc<Bot>,
+    template_directory: String,
+    bot_name: Option<String>,
+}
 
-    dotenv().ok();
-    let bot_name =
-        std::sync::Arc::<String>::from(std::env::var("BOT_NAME").expect("Can't found BOT_NAME"));
-    // need env TELOXIDE_TOKEN
-    // let bot = Bot::from_env();
-    let tg_bot_token = std::env::var("TG_BOT_TOKEN");
-    let bot = Bot::new(tg_bot_token.expect("Can't found TG_BOT_TOKEN"));
-    Dispatcher::builder(
-        bot,
-        dptree::entry().branch(Update::filter_message().branch(
-            dptree::filter(|msg: Message| msg.text().is_some()).endpoint(
-                move |msg: Message, bot: Bot| {
-                    let _bot_name = bot_name.clone();
-                    async move {
-                        if let Some(text) = msg.text() {
-                            if text.starts_with('/') {
-                                match UserCommandType::parse(text, _bot_name.as_ref()) {
-                                    Ok(user_command) => {
-                                        answer(bot, msg, user_command).await?;
-                                    }
-                                    Err(err) => {
-                                        println!("Failed to parse command: {}", err);
+impl TGBotProgram {
+    fn new() -> Self {
+        dotenv().ok();
+        Self::from_token(
+            std::env::var("TG_BOT_TOKEN")
+                .expect("Can't found TG_BOT_TOKEN")
+                .as_str(),
+        )
+    }
+
+    fn from_token(token: &str) -> Self {
+        Self::from_token_and_template(token, "templates")
+    }
+
+    fn from_token_and_template(token: &str, template_directory: &str) -> Self {
+        let bot = std::sync::Arc::new(Bot::new(token));
+        Self {
+            bot,
+            template_directory: template_directory.into(),
+            bot_name: None,
+        }
+    }
+
+    pub async fn run(&mut self) {
+        // teloxide::handler!(println!("{:?}", update));
+        log::info!("Starting telegram bot...");
+        self.bot.set_my_commands(vec![COMMAND_START.clone()]);
+        if (self.bot_name.is_none()) {
+            let _bot_name = self.bot.get_me().await.unwrap().username.unwrap();
+            self.bot_name = Some(_bot_name);
+        }
+        Dispatcher::builder(
+            self.bot,
+            dptree::entry().branch(Update::filter_message().branch(
+                dptree::filter(|msg: Message| msg.text().is_some()).endpoint(
+                    move |msg: Message, bot: Bot| {
+                        let _bot_name = self.bot_name.clone().unwrap();
+                        async move {
+                            if let Some(text) = msg.text() {
+                                if text.starts_with('/') {
+                                    match UserCommandType::parse(text, _bot_name.as_ref()) {
+                                        Ok(user_command) => {
+                                            self.answer( msg, user_command).await?;
+                                        }
+                                        Err(err) => {
+                                            println!("Failed to parse command: {}", err);
+                                        }
                                     }
                                 }
                             }
+                            Result::<(), teloxide::RequestError>::Ok(())
                         }
-                        Result::<(), teloxide::RequestError>::Ok(())
-                    }
-                },
-            ),
-        )),
-    )
-    .build()
-    .dispatch()
-    .await;
-}
-
-async fn answer(bot: Bot, msg: Message, cmd: UserCommandType) -> ResponseResult<()> {
-    match cmd {
-        UserCommandType::Help => {
-            bot.send_message(msg.chat.id, "A command list and introduction.")
-                .await?;
-        }
-        UserCommandType::Start => {
-            let keyboard = InlineKeyboardMarkup::new(vec![vec![InlineKeyboardButton::web_app(
-                "Start It!",
-                WebAppInfo {
-                    url: reqwest::Url::parse("https://t.me/test_tpc_bot/gamehall")
-                        .expect("Failed to parse URL"),
-                },
-            )]]);
-            bot.send_photo(
-                msg.chat.id,
-                InputFile::url(
-                    reqwest::Url::parse("https://i.imgur.com/5y5y5y5.jpg")
-                        .expect("Failed to parse URL"),
+                    },
                 ),
-            )
-            .caption("Welcome to the game hall!")
-            .parse_mode(ParseMode::MarkdownV2)
-            .reply_markup(ReplyMarkup::InlineKeyboard(keyboard))
-            .await?;
-        }
+            )),
+        )
+        .build()
+        .dispatch()
+        .await;
     }
-    Ok(())
+
+    async fn answer(&mut self, msg: Message, cmd: UserCommandType) -> ResponseResult<()> {
+        match cmd {
+            UserCommandType::Help => {
+                self.bot.send_message(msg.chat.id, "A command list and introduction.")
+                    .await?;
+            }
+            UserCommandType::Start => {
+                let keyboard =
+                    InlineKeyboardMarkup::new(vec![vec![InlineKeyboardButton::web_app(
+                        "Start It!",
+                        WebAppInfo {
+                            url: reqwest::Url::parse("https://t.me/test_tpc_bot/gamehall")
+                                .expect("Failed to parse URL"),
+                        },
+                    )]]);
+                self.bot.send_photo(
+                    msg.chat.id,
+                    InputFile::url(
+                        reqwest::Url::parse("https://i.imgur.com/5y5y5y5.jpg")
+                            .expect("Failed to parse URL"),
+                    ),
+                )
+                .caption("Welcome to the game hall!")
+                .parse_mode(ParseMode::MarkdownV2)
+                .reply_markup(ReplyMarkup::InlineKeyboard(keyboard))
+                .await?;
+            }
+        }
+        Ok(())
+    }
 }
