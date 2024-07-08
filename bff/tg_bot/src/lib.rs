b266a25cc0ebc178e1b9c38a766a2092f2f148e1
lotaway <576696294@qq.com>
2024-07-09 00:30:00 +0800

    Add bot call back query(no done)

--- bff/tg_bot/src/lib.rs
+++ bff/tg_bot/src/lib.rs
@@ -1,12 +1,12 @@
 use dotenv::dotenv;
 use dptree::prelude::*;
 use teloxide::dispatching::UpdateFilterExt;
-use teloxide::{prelude::*, ApiError, RequestError};
 use teloxide::types::{
     BotCommand, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, ReplyMarkup, WebAppInfo,
 };
 use teloxide::types::{InputFile, Update};
 use teloxide::utils::command::BotCommands;
+use teloxide::{prelude::*, ApiError, RequestError};
 
 // static COMMAND_START: BotCommand = BotCommand::new("start", "Start the app");
 
@@ -70,9 +70,28 @@ impl TGBotProgram {
             println!("Bot name: {}", _bot_name);
             self.bot_name = Some(_bot_name);
         }
-        let self_arc = std::sync::Arc::new(tokio::sync::Mutex::new(self.clone()));
+        let mut self_arc = std::sync::Arc::new(tokio::sync::Mutex::new(self.clone()));
+        tokio::join![
+            async {
+                let mut self_guard = self_arc.lock().await;
+                self_guard.listen_message(self_arc.clone()).await;
+            },
+            async {
+                let mut self_guard = self_arc.lock().await;
+                self_guard.listen_query(self_arc.clone()).await;
+            },
+        ];
+        println!("Telegram bot stopped.");
+    }
+
+    async fn listen_message(&mut self, self_arc: std::sync::Arc<tokio::sync::Mutex<Self>>) {
+        println!("Start listening message...");
+        // let self_arc = std::sync::Arc::new(tokio::sync::Mutex::new(self.clone()));
+        // let self_arc = self_arc.clone();
+        let bot = self_arc.lock().await.bot.lock().await.clone();
+        let self_arc = self_arc.clone();
         teloxide::dispatching::Dispatcher::builder(
-            self.bot.lock().await.clone(),
+            bot,
             dptree::entry().branch(
                 Update::filter_message().branch(
                     dptree::filter(|msg: Message| {
@@ -103,11 +122,63 @@ impl TGBotProgram {
         )
         .build()
         .dispatch()
-        .await;
-        println!("Telegram bot stopped.");
+        .await
     }
 
-    async fn answer(&mut self, msg: Message, cmd: UserCommandType, mut bot: Bot) -> ResponseResult<()> {
+    async fn listen_query(&mut self, self_arc: std::sync::Arc<tokio::sync::Mutex<Self>>) {
+        println!("Start listening query...");
+        // let self_arc = std::sync::Arc::new(tokio::sync::Mutex::new(self.clone()));
+        let bot = self_arc.lock().await.bot.lock().await.clone();
+        let self_arc = self_arc.clone();
+        teloxide::dispatching::Dispatcher::builder(
+            bot,
+            dptree::entry().branch(
+                Update::filter_callback_query().branch(
+                    dptree::filter(|query: CallbackQuery| {
+                        query.data.is_some()
+                        //  && query.data.unwrap().starts_with('/')
+                    })
+                    .endpoint(move |query: CallbackQuery, bot: Bot| {
+                        println!("Received callback query: {}", query.data.clone().unwrap());
+                        let self_arc = self_arc.clone();
+                        async move {
+                            let self_guard = self_arc.lock().await;
+                            // let mut _self = self_guard.bot.lock().unwrap();
+                            let bot_name = self_guard.bot_name.clone().unwrap();
+                            if let Some(data) = query.data {
+                                match UserCommandType::parse(
+                                    data.clone().as_ref(),
+                                    bot_name.as_ref(),
+                                ) {
+                                    Ok(user_command) => {
+                                        let mut query_message = query.message.clone().unwrap();
+                                        let message =
+                                            bot.send_message(query_message.chat.id, data).await?;
+                                        // self_guard.answer(message, user_command, bot).await?;
+                                        bot.answer_callback_query(query.id).await?;
+                                    }
+                                    Err(err) => {
+                                        println!("Failed to parse command: {}", err);
+                                    }
+                                }
+                            }
+                            Result::<(), teloxide::RequestError>::Ok(())
+                        }
+                    }),
+                ),
+            ),
+        )
+        .build()
+        .dispatch()
+        .await;
+    }
+
+    async fn answer(
+        &mut self,
+        msg: Message,
+        cmd: UserCommandType,
+        mut bot: Bot,
+    ) -> ResponseResult<()> {
         println!(
             "Start to answer command: {:?}, from chat id: {}",
             cmd, msg.chat.id
@@ -118,29 +189,31 @@ impl TGBotProgram {
                     .await
             }
             UserCommandType::Start => {
-                let keyboard =
-                    InlineKeyboardMarkup::new(vec![vec![InlineKeyboardButton::web_app(
+                let keyboard = InlineKeyboardMarkup::new(vec![vec![
+                    InlineKeyboardButton::web_app(
                         "Start It!",
                         WebAppInfo {
                             url: reqwest::Url::parse("https://t.me/test_tpc_bot/gamehall")
                                 .expect("Failed to parse URL"),
                         },
-                    )]]);
+                    ),
+                    InlineKeyboardButton::callback("HELP", "/help"),
+                ]]);
                 bot.send_photo(
-                        msg.chat.id,
-                        InputFile::url(
-                            reqwest::Url::parse("https://i.imgur.com/5y5y5y5.jpg")
-                                .expect("Failed to parse URL"),
-                        ),
-                    )
-                    .caption("Welcome to the game hall\\!")
-                    .parse_mode(ParseMode::MarkdownV2)
-                    .reply_markup(ReplyMarkup::InlineKeyboard(keyboard))
-                    .await
+                    msg.chat.id,
+                    InputFile::url(
+                        reqwest::Url::parse("https://i.imgur.com/5y5y5y5.jpg")
+                            .expect("Failed to parse URL"),
+                    ),
+                )
+                .caption("Welcome to the game hall\\!")
+                .parse_mode(ParseMode::MarkdownV2)
+                .reply_markup(ReplyMarkup::InlineKeyboard(keyboard))
+                .await
             }
-            _ => {
-                Result::Err(RequestError::Api(ApiError::Unknown("Unknown command".into())))
-            }
+            _ => Result::Err(RequestError::Api(ApiError::Unknown(
+                "Unknown command".into(),
+            ))),
         };
         if let Err(error) = send_result {
             println!("Failed to answer command: {:?}, error: {}", cmd, error);
