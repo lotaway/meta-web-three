258166a8fefe4de1bf132888190c8a7291e5cc7d
LOTAWAY\lotaway <576696294@qq.com>
2024-08-03 17:51:06 +0800

    Update contract.

--- solana-contract/src/lib.rs
+++ solana-contract/src/lib.rs
@@ -1,7 +1,17 @@
-use solana_program::{account_info::AccountInfo, entrypoint, entrypoint::{ProgramResult}, pubkey::Pubkey};
+use std::borrow::BorrowMut;
+
+use solana_program::{account_info::{ next_account_info, AccountInfo}, entrypoint::{self, ProgramResult}, msg, pubkey::Pubkey};
 
 entrypoint!(process_instruction);
 fn process_instruction(program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
-    println!("Hello, world!, {}, {:?}, {:?}", program_id, accounts, instruction_data);
+
+    let account = next_account_info(accounts.iter().borrow_mut()).expect("No account found");
+
+    if account.owner != program_id {
+        msg!("Account owner is not the program");
+        return Err(solana_program::program_error::ProgramError::IncorrectProgramId);
+    }
+
+    msg!("Hello, world!, {}, {:?}, {:?}", program_id, accounts, instruction_data);
     Ok(())
 }
