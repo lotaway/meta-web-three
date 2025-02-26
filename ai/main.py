708bef9bd7af21677584b84346c946f0380b321b
LOTAWAY\lotaway <576696294@qq.com>
2025-02-26 17:54:52 +0800

    Fix ai device error.

--- ai/main.py
+++ ai/main.py
@@ -1,9 +1,22 @@
 import torch
+import torch_directml
 
 # check if GPU device work
-if torch.backends.mps.is_available():
-    device = torch.device("mps") # use AMD Metal Performance Shaders
-    print("AMD GPU is available.")
+# if torch.backends.mps.is_available(): # apple silicon
+# if torch.cuda.is_available(): # nvidia
+# if torch.backends.hip.is_available(): # amd
+#     # device = torch.device("mps")
+#     device = torch.device("mps") # use AMD Metal Performance Shaders
+#     print("AMD GPU is available.")
+# else:
+#     device = torch.device("cpu")
+#     print("GPU not available, using CPU.")
+
+if(torch.cuda.is_available()):
+    print("cuda is available")
 else:
-    device = torch.device("cpu")
-    print("GPU not available, using CPU.")
+    print("cuda is not available")
+
+device = torch_directml.device()
+tensor = torch.randn(2, 2).to(device)
+print(tensor)
