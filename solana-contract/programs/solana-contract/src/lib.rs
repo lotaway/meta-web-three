a36103667c03bfca492cba276494b75e12ec25fc
lotaway <576696294@qq.com>
2025-06-05 18:02:39 +0800

    Add deposit and withdraw token in solana contract program.

--- solana-contract/programs/solana-contract/src/lib.rs
+++ solana-contract/programs/solana-contract/src/lib.rs
@@ -1,23 +1,149 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Token, TokenAccount, Transfer, transfer};
+pub mod seeds;
 
 declare_id!("FJBs8eFtXpdnGWcnihfqRJpJTx8Loak4f1pUUAS6nFxn");
 
 #[program]
 pub mod solana_contract {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("solana_contract initialized from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<DepositAccounts>, amount: u64) -> Result<()> {
+        let transaction = Transfer {
+            from: ctx.accounts.sender_token_account.to_account_info(),
+            to: ctx.accounts.program_token_account.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            transaction,
+        );
+        transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<WithdrawAccounts>, amount: u64) -> Result<()> {
+        let transaction = Transfer {
+            from: ctx.accounts.program_token_account.to_account_info(),
+            to: ctx.accounts.recevier_token_account.to_account_info(),
+            authority: ctx.accounts.token_mananer.to_account_info(),
+        };
+        let seeds = [seeds::TOKEN_MANAGER, ctx.accounts.token_mint_account.key().as_ref()];
+        let bump = ctx.accounts.token_manager.bump;
+        let signer = &[&seeds[..]];
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transaction,
+            signer,
+        );
+        transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = admin, space = 8 + 8)]
+
+    #[account(
+        init_if_needed,
+        payer = signer, 
+        seeds=[seeds::TOKEN_MANAGER], 
+        bump,
+        space = 8,
+    )]
     token_manager: AccountInfo<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = signer,
+        seeds=[seeds::PROGRAM_TOKEN_ACCOUNT, token_mint_account.key().as_ref()],
+        bump,
+        space = TokenAccount::LEN,
+    )]
+    program_token_account: Account<'info, TokenAccount>,
+
+    token_mint_account: AccountInfo<'info>,
+
     #[account(mut)]
-    admin: Signer<'info>,
+    signer: Signer<'info>,
+
     system_program: Program<'info, System>,
+
+    token_program: Program<'info, Token>,
+    
+    rent: Sysvar<'info, Rent>,
 }
 
+#[derive(Accounts)]
+pub struct DepositAccounts<'info> {
+
+    #[account(
+        mut, 
+        seeds=[seeds::TOKEN_MANAGER], 
+        bump,
+    )]
+    token_manager: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        seeds=[seeds::PROGRAM_TOKEN_ACCOUNT, token_mint_account.key().as_ref()],
+        bump,
+        token::mint = token_mint_account,
+        token::authority = token_manager,
+    )]
+    program_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    sender_token_account: AccountInfo<'info>,
+
+    token_mint_account: AccountInfo<'info>,
+
+    #[account(mut)]
+    signer: Signer<'info>,
+
+    system_program: Program<'info, System>,
+
+    token_program: Program<'info, Token>,
+    
+    rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct WithDrawAccounts<'info> {
+
+    #[account(
+        mut, 
+        seeds=[seeds::TOKEN_MANAGER], 
+        bump,
+    )]
+    token_manager: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        seeds=[seeds::PROGRAM_TOKEN_ACCOUNT, token_mint_account.key().as_ref()],
+        bump,
+        token::mint = token_mint_account,
+        token::authority = token_manager,
+    )]
+    program_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    recevier_token_account: AccountInfo<'info>,
+
+    token_mint_account: AccountInfo<'info>,
+
+    #[account(mut)]
+    signer: Signer<'info>,
+
+    system_program: Program<'info, System>,
+
+    token_program: Program<'info, Token>,
+    
+    rent: Sysvar<'info, Rent>,
+}
---
+++ solana-contract/programs/solana-contract/src/seeds.rs
@@ -0,0 +1,2 @@
+pub const TOKEN_MANAGER: &[u8] = b"token_manager";
+pub const PROGRAM_TOKEN_ACCOUNT : &[u8] = b"program_token_account";
