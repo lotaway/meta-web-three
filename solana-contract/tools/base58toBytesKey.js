5b38d4edbd29e21b1f1392a171e5377b676091f1
lotaway <576696294@qq.com>
2025-06-15 00:07:31 +0800

    Update config.

--- solana-contract/tools/base58toBytesKey.js
+++ solana-contract/tools/base58toBytesKey.js
@@ -1,7 +1,7 @@
 const bs58 = require('bs58')
 const fs = require('fs')
 
-const secretKeyBase58 = '4EgqssMAf1gLcrKvn3btqQZhcgW2BH6vVYsZv9yaQz6xj4fTyT17FksxL7kBjLBfd3vmcJDHuhxryPNNmD78u8EK'  // 你的 base58 私钥字符串
+const secretKeyBase58 = ''  // 你的 base58 私钥字符串
 const secretKey = bs58.decode(secretKeyBase58)
 
 fs.writeFileSync('id.json', JSON.stringify(Array.from(secretKey)))
