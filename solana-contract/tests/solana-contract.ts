23cac8423d1339a692986d6db7eb2d84a4274304
lotaway <576696294@qq.com>
2025-06-15 21:16:56 +0800

    Fix error in test init.

--- solana-contract/tests/solana-contract.ts
+++ solana-contract/tests/solana-contract.ts
@@ -1,21 +1,29 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaContract } from "../target/types/solana_contract";
-import { Keypair } from "@solana/web3.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
 import { keypairIdentity, Metaplex } from "@metaplex-foundation/js";
 
 describe("solana-contract", () => {
   // Configure the client to use the local cluster.
 
-  let program
+  let program: Program<SolanaContract>
   let createdNFT
 
   before(async () => {
     anchor.setProvider(anchor.AnchorProvider.env());
 
     program = anchor.workspace.solanaContract as Program<SolanaContract>;
-    
-    const tx = await program.methods.initialize().rpc();
+
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        tokenMintAccount: PublicKey.findProgramAddressSync([
+          Buffer.from("token_manager"),
+        ], program.programId)[0],
+        // signer
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 
