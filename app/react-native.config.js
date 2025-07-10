9bc010a47630cb2635e9dc2be16a33a5d311adcd
lotaway <576696294@qq.com>
2025-07-10 22:25:43 +0800

    Update app module(no fix).

--- app/react-native.config.js
+++ app/react-native.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 const AppSdkPkg = require('../AppSdk/package.json');
 
+const getDirname = () => {
+  try {
+    return __dirname;
+  } catch {
+    return path.dirname(require.main.filename);
+  }
+};
+
 module.exports = {
   project: {
     ios: {
@@ -9,7 +17,7 @@ module.exports = {
   },
   dependencies: {
     [AppSdkPkg.name]: {
-      root: path.join(__dirname, '../AppSdk'),
+      root: path.join(getDirname(), '../AppSdk'),
       platforms: {
         // Codegen script incorrectly fails without this
         // So we explicitly specify the platforms with empty object
