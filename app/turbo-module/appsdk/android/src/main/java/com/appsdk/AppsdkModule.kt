63a733144f3ccbc540bb1f20c269b72b51ebf5c3
lotaway <576696294@qq.com>
2025-07-16 16:56:32 +0800

    Fix android app deploy error

--- app/turbo-module/appsdk/android/src/main/java/com/appsdk/AppsdkModule.kt
+++ app/turbo-module/appsdk/android/src/main/java/com/appsdk/AppsdkModule.kt
@@ -2,6 +2,7 @@ package com.appsdk
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.bridge.Promise
 
 @ReactModule(name = AppsdkModule.NAME)
 class AppsdkModule(reactContext: ReactApplicationContext) :
