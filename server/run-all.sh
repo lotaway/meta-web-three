ad3f692b818f89efec987726b2a3f93a411ed4d2
lotaway <576696294@qq.com>
2025-07-28 15:56:38 +0800

    Cross micro service call done.

--- server/run-all.sh
+++ server/run-all.sh
@@ -1,4 +1,5 @@
 # run-all.sh
+docker run -d -name zookeeper -p 2181:2181 --restart unless-stopped zookeeper:3.7 &
 cd product-service && mvn spring-boot:run &
 cd user-service && mvn spring-boot:run &
 cd message-service && mvn spring-boot:run &
