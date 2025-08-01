1325aff1250d49b0f5352868179a47782b5234e0
lotaway <576696294@qq.com>
2025-08-01 13:42:55 +0800

    Update user service token.

---
+++ server/common/src/main/java/com/metawebthree/common/utils/JwtUtil.java
@@ -0,0 +1,89 @@
+package com.metawebthree.common.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.security.SignatureException;
+import java.util.Date;
+import java.util.Optional;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret:123123}")
+    private String secret;
+
+    @Autowired
+    private String name;
+
+    public String generate(String subject) {
+        return Jwts.builder()
+                .setHeaderParam(name, "JWT")
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    public String generate(Long userId) {
+        return generate(userId.toString());
+    }
+
+    protected Key getSignKey() {
+        return new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.ES512.getJcaName());
+    }
+
+    public Claims decode(String token) throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException,
+            SignatureException, IllegalArgumentException {
+        JwtParserBuilder builder = Jwts.parserBuilder();
+        builder.setSigningKey(getSignKey());
+        JwtParser parser = builder.build();
+        return parser.parseClaimsJws(token).getBody();
+    }
+
+    public Optional<Claims> tryDecode(String token) {
+        try {
+            return Optional.of(decode(token));
+        } catch (Throwable e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<String> getSubject(String token) {
+        return tryDecode(token).map(Claims::getSubject);
+    }
+
+    public Optional<Long> getUserId(String token) {
+        return tryDecode(token).map(Claims::getSubject).map(Long::parseLong);
+    }
+
+    public Optional<Date> getExpiration(String token) {
+        return tryDecode(token).map(Claims::getExpiration);
+    }
+
+    public boolean isTokenExpired(Date expiration) {
+        return expiration.before(new Date());
+    }
+
+    public boolean isTokenNotExpired(Date expiration) {
+        return !isTokenExpired(expiration);
+    }
+
+    public boolean isTokenValid(String token) {
+        return tryDecode(token).map(Claims::getExpiration).map(this::isTokenNotExpired).orElse(false);
+    }
+
+}
