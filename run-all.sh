78d1a4a74dd044d4d6fa66040373aa9c9c32b416
lotaway <576696294@qq.com>
2025-08-18 01:16:36 +0800

    Update run script.

--- run-all.sh
+++ run-all.sh
@@ -1,9 +1,4 @@
 # run-all.sh
 ./run-env.sh &
-cd server/product-service && mvn spring-boot:run &
-cd server/user-service && mvn spring-boot:run &
-cd server/message-service && mvn spring-boot:run &
-cd server/order-service && mvn spring-boot:run &
-cd server/payment-service && mvn spring-boot:run &
-cd server/media-service && mvn spring-boot:run &
+./run-server.sh &
 ./run-client.sh
---
+++ run-server.sh
@@ -0,0 +1,8 @@
+cd server && mvn clean install &
+cd server/common && mvn clean install &
+cd server/product-service && mvn spring-boot:run &
+cd server/user-service && mvn spring-boot:run &
+cd server/message-service && mvn spring-boot:run &
+cd server/order-service && mvn spring-boot:run &
+cd server/payment-service && mvn spring-boot:run &
+cd server/media-service && mvn spring-boot:run
