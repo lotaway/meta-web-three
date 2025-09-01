21946b72cf4334dc38e14fce01b63d572591adb5
lotaway <576696294@qq.com>
2025-09-02 01:37:42 +0800

    Update log config.

--- server/common/src/main/java/com/metawebthree/common/config/LogRocksDBAppender.java
+++ server/common/src/main/java/com/metawebthree/common/config/LogRocksDBAppender.java
@@ -9,15 +9,24 @@ import org.rocksdb.RocksDBException;
 public class LogRocksDBAppender extends AppenderBase<ILoggingEvent> {
     private RocksDB db;
     private static long seq = 0;
+    private static final String SEQ_KEY = "SystemOut";
+
+    private final String LOG_PATH = "/logs/logs-rocksdb";
 
     @Override
     public void start() {
-        try {
-            RocksDB.loadLibrary();
-            try (Options options = new Options().setCreateIfMissing(true)) {
-                db = RocksDB.open(options, "/tmp/logs-rocksdb");
-                super.start();
+        RocksDB.loadLibrary();
+        try (Options options = new Options().setCreateIfMissing(true)) {
+            db = RocksDB.open(options, LOG_PATH);
+            byte[] preSeq = db.get(SEQ_KEY.getBytes());
+            if (preSeq != null && preSeq.length > 0) {
+                try {
+                    seq = Long.parseLong(new String(preSeq));
+                } catch (NumberFormatException e) {
+                    // do nothing
+                }
             }
+            super.start();
         } catch (RocksDBException e) {
             addError("Failed to open RocksDB", e);
         }
@@ -30,8 +39,9 @@ public class LogRocksDBAppender extends AppenderBase<ILoggingEvent> {
             String level = eventObject.getLevel().toString();
             String thread = eventObject.getThreadName();
             String log = String.format("[%s] [%s] %s", level, thread, message);
-
-            byte[] key = String.valueOf(seq++).getBytes();
+            byte[] seqKey = SEQ_KEY.getBytes();
+            db.put(seqKey, String.valueOf(seq).getBytes());
+            byte[] key = String.format("%s:%d", SEQ_KEY, seq++).getBytes();
             db.put(key, log.getBytes());
         } catch (Exception e) {
             addError("Error writing log to RocksDB", e);
