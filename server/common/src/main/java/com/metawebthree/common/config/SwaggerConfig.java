a234879b0f791b59732d64d43d4bf8a11460f056
lotaway <576696294@qq.com>
2025-09-03 13:36:06 +0800

    Update swagger config for micro service.

--- server/common/src/main/java/com/metawebthree/common/config/SwaggerConfig.java
+++ server/common/src/main/java/com/metawebthree/common/config/SwaggerConfig.java
@@ -4,25 +4,27 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Meta Web Three - Service API")
-                        .version("1.0.0")
-                        .description("API documentation for the Service of Meta Web Three platform")
-                        .contact(new Contact()
-                                .name("Meta Web Three Team")
-                                .email("support@metawebthree.com")
-                                .url("https://metawebthree.com"))
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("Meta Web Three - Service API")
+                                                .version("1.0.0")
+                                                .description(System.getProperty("spring.boot.project.name", "")
+                                                                + "API documentation for the Service of Meta Web Three platform")
+                                                .contact(new Contact()
+                                                                .name("Meta Web Three Team")
+                                                                .email("support@metawebthree.com")
+                                                                .url("https://metawebthree.com"))
+                                                .license(new License()
+                                                                .name("MIT License")
+                                                                .url("https://opensource.org/licenses/MIT")));
+        }
 }
