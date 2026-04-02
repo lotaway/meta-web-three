40a86fa0b0bb52d12bee1857463894be1bec74aa
lotaway <576696294@qq.com>
2026-04-02 21:39:47 +0800

    Fix error in server run.

--- server/media-service/src/main/java/com/metawebthree/media/interfaces/web/MediaController.java
+++ server/media-service/src/main/java/com/metawebthree/media/interfaces/web/MediaController.java
@@ -1,4 +1,5 @@
 package com.metawebthree.media.interfaces.web;
+
 import com.metawebthree.media.application.*;
 import com.metawebthree.media.application.dto.*;
 
@@ -14,7 +15,6 @@ import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 
 @RestController
 @RequestMapping("/media")
@@ -39,34 +39,6 @@ public class MediaController {
     public ApiResponse<Void> file(@RequestParam("file") MultipartFile file) {
         String fileName = file.getOriginalFilename();
         try {
-            File destFile = new File(uploadPath + fileName);
-            FileUtils.writeByteArrayToFile(destFile, file.getBytes());
-        } catch (IOException e) {
-            return ApiResponse.error(ResponseStatus.MEDIA_UPLOAD_FAILED);
-        }
-        return ApiResponse.success();
-    }
-
-    @GetMapping("/{key}")
-    public byte[] getMedia(@PathVariable String key) {
-        return mediaService.getMedia(key).orElse(new byte[]{});
-    }
-
-    @DeleteMapping("/{key}")
-    public void deleteMedia(@PathVariable String key) {
-        mediaService.deleteMedia(key);
-    }
-}
-
-    @PostMapping
-    public String uploadMedia(@RequestParam("file") MultipartFile file) {
-        return mediaService.uploadFile(file);
-    }
-
-    @PostMapping("/upload/file")
-    public ApiResponse<Void> file(@RequestParam("file") MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        try {
             // TODO: Avoid hardcoded paths
             File destFile = new File("/upload/file/" + fileName);
             FileUtils.writeByteArrayToFile(destFile, file.getBytes());
@@ -75,11 +47,10 @@ public class MediaController {
         }
         return ApiResponse.success();
     }
-    
 
     @GetMapping("/{key}")
     public byte[] getMedia(@PathVariable String key) {
-        return mediaService.getMedia(key).orElse(new byte[]{});
+        return mediaService.getMedia(key).orElse(new byte[] {});
     }
 
     @DeleteMapping("/{key}")
