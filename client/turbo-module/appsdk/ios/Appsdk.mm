611adccf73c1903f0ea94e7652d2efb6db8a1c7a
lotaway <576696294@qq.com>
2026-04-15 22:26:03 +0800

    Update ios client passkey implement.

--- client/turbo-module/appsdk/ios/Appsdk.mm
+++ client/turbo-module/appsdk/ios/Appsdk.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <CommonCrypto/CommonDigest.h>
 #import <CommonCrypto/CommonHMAC.h>
+#import <AuthenticationServices/AuthenticationServices.h>
 
 @implementation Appsdk
 
@@ -12,15 +13,13 @@ RCT_EXPORT_MODULE()
   return dispatch_get_main_queue();
 }
 
-
-
 - (NSString *)generateRequestSignature:(NSDictionary *)params secretKey:(NSString *)secretKey {
   NSArray *sortedKeys = [[params allKeys] sortedArrayUsingSelector:@selector(compare:)];
   NSMutableArray *pairs = [NSMutableArray array];
   for (NSString *key in sortedKeys) {
     [pairs addObject:[NSString stringWithFormat:@"%@=%@", key, params[key]]];
   }
-  NSString *message = [[pairs componentsJoinedByString:@"&"] stringByAppendingString:secretKey];
+  NSString *message = [[pairs componentsJoinedByString:@\"&\"] stringByAppendingString:secretKey];
   
   unsigned char digest[CC_SHA256_DIGEST_LENGTH];
   NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
@@ -43,7 +42,8 @@ RCT_EXPORT_MODULE()
 
 - (NSString *)computeOrderTotal:(NSString *)unitPrice quantity:(double)quantity discountAmount:(NSString *)discountAmount shippingFee:(NSString *)shippingFee {
   NSDecimalNumber *price = [NSDecimalNumber decimalNumberWithString:unitPrice];
-  NSDecimalNumber *subtotal = [price decimalNumberByMultiplyingBy: [NSDecimalNumber decimalNumberWithMantissa:quantity exponent:0 isNegative:YES]];
+  NSDecimalNumber *qty = [NSDecimalNumber decimalNumberWithMantissa:quantity exponent:0 isNegative:NO];
+  NSDecimalNumber *subtotal = [price decimalNumberByMultiplyingBy:qty];
   NSDecimalNumber *discount = [NSDecimalNumber decimalNumberWithString:discountAmount];
   NSDecimalNumber *shipping = [NSDecimalNumber decimalNumberWithString:shippingFee];
   NSDecimalNumber *total = [[subtotal decimalNumberBySubtracting:discount] decimalNumberByAdding:shipping];
@@ -77,20 +77,37 @@ RCT_EXPORT_MODULE()
   return @([[NSDate date] timeIntervalSince1970] * 1000);
 }
 
+RCT_EXPORT_METHOD(createPasskey:(NSString *)rpId userName:(NSString *)userName 
+  resolver:(RCTPromiseResolveBlock)resolve 
+  rejecter:(RCTPromiseRejectBlock)reject)
+
 - (void)createPasskey:(NSString *)rpId userName:(NSString *)userName resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
-  NSString *passkey = [NSString stringWithFormat:@"passkey-%@", [self createNonce]];
-  resolve(passkey);
+  NSString *mockResponse = [NSString stringWithFormat:@"{\\"id\\": \\"%@\\", \\"type\\": \\"public-key\\"}", [self createNonce]];
+  resolve(mockResponse);
 }
 
-- (NSArray *)getPasskeyList {
-  return @[@"passkey-1", @"passkey-2"];
+RCT_EXPORT_METHOD(getPasskeyList:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+- (void)getPasskeyList:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+  NSArray *list = @[@"passkey-1", @"passkey-2"];
+  resolve(list);
 }
+
+RCT_EXPORT_METHOD(authenticatePasskey:(NSString *)challenge 
+  resolver:(RCTPromiseResolveBlock)resolve 
+  rejecter:(RCTPromiseRejectBlock)reject)
 
 - (void)authenticatePasskey:(NSString *)challenge resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
-  resolve(@YES);
+  NSString *mockResponse = @"{\\"id\\": \\"passkey-1\\", \\"type\\": \\"public-key\\", \\"response\\": {\\"clientDataJSON\\": \\"mock_client_data\\"}}";
+  resolve(mockResponse);
 }
 
-- (void)deletePasskey:(NSString *)credentialId {
+RCT_EXPORT_METHOD(deletePasskey:(NSString *)credentialId 
+  resolver:(RCTPromiseResolveBlock)resolve 
+  rejecter:(RCTPromiseRejectBlock)reject)
+
+- (void)deletePasskey:(NSString *)credentialId resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+  resolve(@NO);
 }
 
 @end
