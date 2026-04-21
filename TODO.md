1d705ed310d324982cac2864ddb15c86a8266881
lotaway <576696294@qq.com>
2026-04-21 18:26:31 +0800

    Fix user service error.

--- TODO.md
+++
@@ -1,9 +0,0 @@
-# Appsdk.mm 完善计划 (基于 Android AppsdkModule.kt)
-
-## 步骤列表
-- [ ] 步骤1: 创建 TODO.md (已完成)
-- [x] 步骤2: 升级 Appsdk.mm - 匹配同步方法算法 + 实现真 Passkey (AuthenticationServices)
-- [x] 步骤3: 测试 iOS build (运行 pod install & build)
-- [x] 步骤4: 完成任务
-
-当前进度: 步骤1 完成。
--- server/user-service/src/main/java/com/metawebthree/user/UserServiceApplication.java
+++ server/user-service/src/main/java/com/metawebthree/user/UserServiceApplication.java
@@ -1,13 +1,15 @@
 package com.metawebthree.user;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import com.metawebthree.common.BaseApplication;
 
 @SpringBootApplication
-
 @EnableDiscoveryClient
-public class UserServiceApplication {
+@EnableDubbo
+public class UserServiceApplication extends BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
