9e631e760079fa38696408b2bd6896eed9d6410a
lotaway <576696294@qq.com>
2026-04-21 14:58:36 +0800

    Fix cart and promotion compile error.

--- server/promotion-service/src/main/java/com/metawebthree/promotion/PromotionServiceApplication.java
+++ server/promotion-service/src/main/java/com/metawebthree/promotion/PromotionServiceApplication.java
@@ -1,12 +1,17 @@
 package com.metawebthree.promotion;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+import com.metawebthree.common.BaseApplication;
+
+@SpringBootApplication(exclude = QuartzAutoConfiguration.class)
 @EnableDiscoveryClient
-public class PromotionServiceApplication {
+@EnableDubbo
+public class PromotionServiceApplication extends BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(PromotionServiceApplication.class, args);
     }
