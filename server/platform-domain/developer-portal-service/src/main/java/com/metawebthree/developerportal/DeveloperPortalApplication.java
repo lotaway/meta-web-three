63cbe38a1cb539df57b122035f580a65f811d331
LOTAWAY\lotaway <576696294@qq.com>
2026-07-23 09:41:54 +0800

    Fix developer portal service startup error.

---
+++ server/platform-domain/developer-portal-service/src/main/java/com/metawebthree/developerportal/DeveloperPortalApplication.java
@@ -0,0 +1,17 @@
+package com.metawebthree.developerportal;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import com.metawebthree.common.BaseApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableDubbo
+public class DeveloperPortalApplication extends BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperPortalApplication.class, args);
+    }
+}
